5885fd187143c395c4c777adf09c1f8ca57ea543
Rob DeCarlo <rdecarlo73@gmail.com>
2014-05-06 07:05:52 -0400

    Initial Commit

---
+++ CPPTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPPTest
+//
+//  Created by Rob DeCarlo on 5/6/14.
+//  Copyright (c) 2014 Rob DeCarlo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
